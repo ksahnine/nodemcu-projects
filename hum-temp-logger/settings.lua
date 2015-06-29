86e77321f591b1827118ca129597ca2ee6d4225f
Kadda SAHNINE <ksahnine@inoviabook.local>
2015-06-29 18:02:14 +0200

    Suppression répertoire

--- hum-temp-logger/settings.lua
+++
@@ -1,9 +0,0 @@
--- Modify the Wifi access point ssid and password
-ssid = "foobar"
-passwd = "XXXXXXXXXXX"
--- DHT11 data pin (GPIO02)
-pin = 4
--- ThingSpeak API key
-apikey = "YYYYYYYYYYYYY"
--- Delay in milliseconds between 2 queries to ThingSpeak
-publish_delay = 20000
